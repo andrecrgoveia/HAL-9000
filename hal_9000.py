ad82d02db9e93c2a59cf4ddd651eeee260dc9440
André Goveia <andrecrgoveia@gmail.com>
2020-06-18 23:12:57 -0300

    Back to chatterbot

--- hal_9000.py
+++ hal_9000.py
@@ -1,3 +1,17 @@
-import nltk
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
 
-nltk.download()
+bot = ChatBot('HAL 9000')
+
+conversa = []
+
+bot.set_trainer(ListTrainer)
+bot.train(conversa)
+
+while True:
+    pergunta = input('VocÊ')
+    resposta = bot.get_response(pergunta)
+    if float(resposta.confidence) > 0.5:
+        print('HAL 9000: ', resposta)
+    else:
+        print('HAL 9000: ')
