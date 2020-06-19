9937f254e5ac1a4423dc302f5c848775a4ef0731
André Goveia <andrecrgoveia@gmail.com>
2020-06-18 22:40:16 -0300

    testing nltk

--- hal_9000.py
+++ hal_9000.py
@@ -1,12 +1,3 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+import nltk
 
-hal_9000 = ChatBot(name='HAL 9000', read_only=True)
-
-small_talk = ['Olá!', 'Oi', 'Como você está?', 'Eu estou bem', 'Tudo blz né?', 'Sempre!', 'Qual seu nome?', 'HAL 9000']
-
-list_trainer = ListTrainer(hal_9000)
-
-for intem in(small_talk):
-    list_trainer.train(item)
-    print(hal_9000.get_response('Olá'))
+nltk.download()
